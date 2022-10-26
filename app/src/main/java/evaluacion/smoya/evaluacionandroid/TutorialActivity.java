8947d56d478efb82b0c6c83dac478b12ccb484cd
smoya13 <s.moya13@alumnos.santotomas.cl>
2022-10-26 03:08:27 -0300

    🗺Implementando MapBox a la APP

--- app/src/main/java/evaluacion/smoya/evaluacionandroid/TutorialActivity.java
+++ app/src/main/java/evaluacion/smoya/evaluacionandroid/TutorialActivity.java
@@ -8,17 +8,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 import java.util.ArrayList;
 
 public class TutorialActivity extends AppCompatActivity {
     Button btnOk;
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
         btnOk = findViewById(R.id.btnEntendido);
-
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
 
     }
 
